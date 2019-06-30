cc07805d15a9b69417a2840e0b9d8db7e1e05ff2
Draylar <samuelhimes@gmail.com>
2019-06-30 18:17:03 -0500

    1.14.3 version bump

--- src/main/java/com/github/draylar/modid/ExampleMod.java
+++ src/main/java/com/github/draylar/modid/ExampleMod.java
@@ -2,6 +2,7 @@ package com.github.draylar.modid;
 
 import com.github.draylar.modid.common.Blocks;
 import com.github.draylar.modid.common.Items;
+import com.github.draylar.modid.common.Entities;
 import net.fabricmc.api.ModInitializer;
 
 public class ExampleMod implements ModInitializer
@@ -11,5 +12,6 @@ public class ExampleMod implements ModInitializer
 	{
 		Blocks.init();
 		Items.init();
+		Entities.register();
 	}
 }
