23eed8e03df8af3bbb8bbdff18ccdead810e32b7
Draylar <samuelhimes@gmail.com>
2019-10-10 13:44:58 -0500

    values put into properties file, default deps added, latest MC version

---
+++ src/main/java/com/github/draylar/modid/config/ModConfig.java
@@ -0,0 +1,10 @@
+package com.github.draylar.modid.config;
+
+import com.github.draylar.modid.ExampleMod;
+import me.sargunvohra.mcmods.autoconfig1.ConfigData;
+import me.sargunvohra.mcmods.autoconfig1.annotation.Config;
+
+@Config(name = ExampleMod.MODID)
+public class ModConfig implements ConfigData {
+
+}
