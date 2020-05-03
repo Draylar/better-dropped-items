0a4fefb6d0c6541716eb4d1e54f8a1a71aeb4492
Draylar <samuelhimes@gmail.com>
2020-05-03 12:17:13 -0500

    Improve item stack rendering, make location consistent with hitbox, version bump to 1.1.0, dep upgrades

--- src/main/java/bdi/mixin/ItemEntityRendererMixin.java
+++ src/main/java/bdi/mixin/ItemEntityRendererMixin.java
@@ -45,6 +45,7 @@ public abstract class ItemEntityRendererMixin extends EntityRenderer<ItemEntity>
     private void render(ItemEntity itemEntity, float f, float partialTicks, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo callback) {
         ItemStack itemStack = itemEntity.getStack();
 
+        // setup seed for random rotation
         int seed = itemStack.isEmpty() ? 187 : Item.getRawId(itemStack.getItem()) + itemStack.getDamage();
         this.random.setSeed(seed);
 
@@ -52,10 +53,13 @@ public abstract class ItemEntityRendererMixin extends EntityRenderer<ItemEntity>
         BakedModel bakedModel = this.itemRenderer.getHeldItemModel(itemStack, itemEntity.world, null);
         boolean hasDepthInGui = bakedModel.hasDepth();
 
+        // decide how many item layers to render
         int renderCount = this.getRenderedAmount(itemStack);
 
+        // make item lie flat on ground
         matrixStack.multiply(Vector3f.POSITIVE_X.getRadialQuaternion(1.571F));
 
+        // rotate item
         ItemEntityRotator rotator = (ItemEntityRotator) itemEntity;
         if(!itemEntity.onGround && !itemEntity.isSubmergedInWater()) {
             float rotation = ((float)itemEntity.getAge() + partialTicks) / 20.0F + itemEntity.hoverHeight;
@@ -65,49 +69,61 @@ public abstract class ItemEntityRendererMixin extends EntityRenderer<ItemEntity>
         else {
             matrixStack.multiply(Vector3f.POSITIVE_Z.getRadialQuaternion((float) rotator.getRotation().z));
         }
-        
-        matrixStack.translate(-0.07f, 0, -0.01f);
+
+        // offset rendered stacks vertically
+        // 0.01 barely brings flat items flush to the ground
+        matrixStack.translate(0, 0, -0.01f);
         if(itemEntity.getStack().getItem() instanceof BlockItem) {
-            matrixStack.translate(0.23f, 0, -0.14f);
+            // make blocks flush with the ground
+            matrixStack.translate(0, 0, -0.12f);
         }
 
         float scaleX = bakedModel.getTransformation().ground.scale.getX();
         float scaleY = bakedModel.getTransformation().ground.scale.getY();
         float scaleZ = bakedModel.getTransformation().ground.scale.getZ();
 
-        float v;
-        float w;
+        float x;
+        float y;
         if (!hasDepthInGui) {
-            float r = -0.0F * (float)(renderCount - 1) * 0.5F * scaleX;
-            v = -0.0F * (float)(renderCount - 1) * 0.5F * scaleY;
-            w = -0.09375F * (float)(renderCount - 1) * 0.5F * scaleZ;
-            matrixStack.translate((double)r, (double)v, (double)w);
+            float r = -0.0F * (float)(renderCount) * 0.5F * scaleX;
+            x = -0.0F * (float)(renderCount) * 0.5F * scaleY;
+            y = -0.09375F * (float)(renderCount) * 0.5F * scaleZ;
+            matrixStack.translate(r, x, y);
         }
 
+        // render each item in the stack on the ground (higher stack count == more items displayed)
         for(int u = 0; u < renderCount; ++u) {
             matrixStack.push();
+
+            // random positioning for rendered items, is especially seen in 64 block stacks on the ground
             if (u > 0) {
                 if (hasDepthInGui) {
-                    v = (this.random.nextFloat() * 2.0F - 1.0F) * 0.15F;
-                    w = (this.random.nextFloat() * 2.0F - 1.0F) * 0.15F;
-                    float x = (this.random.nextFloat() * 2.0F - 1.0F) * 0.15F;
-                    matrixStack.translate((double)v, (double)w, (double)x);
+                    x = (this.random.nextFloat() * 2.0F - 1.0F) * 0.15F;
+                    y = (this.random.nextFloat() * 2.0F - 1.0F) * 0.15F;
+                    float z = (this.random.nextFloat() * 2.0F - 1.0F) * 0.15F;
+                    matrixStack.translate(x, y, z);
                 } else {
-                    v = (this.random.nextFloat() * 2.0F - 1.0F) * 0.15F * 0.5F;
-                    w = (this.random.nextFloat() * 2.0F - 1.0F) * 0.15F * 0.5F;
-                    matrixStack.translate((double)v, (double)w, 0.0D);
+                    x = (this.random.nextFloat() * 2.0F - 1.0F) * 0.15F * 0.5F;
+                    y = (this.random.nextFloat() * 2.0F - 1.0F) * 0.15F * 0.5F;
+                    matrixStack.translate(x, y, 0.0D);
+                    matrixStack.multiply(Vector3f.POSITIVE_Z.getRadialQuaternion(this.random.nextFloat()));
                 }
             }
 
+            // render item
             this.itemRenderer.renderItem(itemStack, ModelTransformation.Mode.GROUND, false, matrixStack, vertexConsumerProvider, i, OverlayTexture.DEFAULT_UV, bakedModel);
+
+            // end
             matrixStack.pop();
+
+            // translate based on scale, which gies vertical layering to high stack count items
             if (!hasDepthInGui) {
-                matrixStack.translate((double)(0.0F * scaleX), (double)(0.0F * scaleY), (double)(0.09375F * scaleZ));
+                matrixStack.translate(0.0F * scaleX, 0.0F * scaleY, 0.0625F * scaleZ);
             }
         }
 
+        // end
         matrixStack.pop();
-        
         super.render(itemEntity, f, partialTicks, matrixStack, vertexConsumerProvider, i);
         callback.cancel();
     }
