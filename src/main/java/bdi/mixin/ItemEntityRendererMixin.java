29a194d694f36913a1b1cf1fade2617308650539
Draylar <samuelhimes@gmail.com>
2020-11-10 17:21:05 -0600

    slightly fix water bobbing

--- src/main/java/bdi/mixin/ItemEntityRendererMixin.java
+++ src/main/java/bdi/mixin/ItemEntityRendererMixin.java
@@ -1,16 +1,9 @@
 package bdi.mixin;
 
-import java.util.Random;
-
 import bdi.util.ItemEntityRotator;
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
+import net.minecraft.block.ShapeContext;
 import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
@@ -22,19 +15,32 @@ import net.minecraft.client.render.model.json.ModelTransformation;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.util.math.Vector3f;
 import net.minecraft.entity.ItemEntity;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.AliasedBlockItem;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tag.FluidTags;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.shape.VoxelShape;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Random;
 
 @Mixin(ItemEntityRenderer.class)
 public abstract class ItemEntityRendererMixin extends EntityRenderer<ItemEntity> {
+
     @Shadow @Final private Random random;
     @Shadow @Final private ItemRenderer itemRenderer;
     @Shadow protected abstract int getRenderedAmount(ItemStack stack);
 
-    protected ItemEntityRendererMixin(EntityRenderDispatcher dispatcher) {
+    private ItemEntityRendererMixin(EntityRenderDispatcher dispatcher) {
         super(dispatcher);
     }
 
@@ -44,52 +50,122 @@ public abstract class ItemEntityRendererMixin extends EntityRenderer<ItemEntity>
     }
 
     @Inject(at = @At("HEAD"), method = "render", cancellable = true)
-    private void render(ItemEntity itemEntity, float f, float partialTicks, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo callback) {
-        ItemStack itemStack = itemEntity.getStack();
+    private void render(ItemEntity dropped, float f, float partialTicks, MatrixStack matrix, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo callback) {
+        ItemStack itemStack = dropped.getStack();
 
         // setup seed for random rotation
         int seed = itemStack.isEmpty() ? 187 : Item.getRawId(itemStack.getItem()) + itemStack.getDamage();
         this.random.setSeed(seed);
 
-        matrixStack.push();
-        BakedModel bakedModel = this.itemRenderer.getHeldItemModel(itemStack, itemEntity.world, null);
+        matrix.push();
+        BakedModel bakedModel = this.itemRenderer.getHeldItemModel(itemStack, dropped.world, null);
         boolean hasDepthInGui = bakedModel.hasDepth();
 
         // decide how many item layers to render
         int renderCount = this.getRenderedAmount(itemStack);
 
-        // make item lie flat on ground
-        matrixStack.multiply(Vector3f.POSITIVE_X.getRadialQuaternion(1.571F));
+        // Helper for manipulating data on the current ItemEntity
+        ItemEntityRotator rotator = (ItemEntityRotator) dropped;
 
-        // rotate item
-        ItemEntityRotator rotator = (ItemEntityRotator) itemEntity;
-        if(!itemEntity.isOnGround() && !itemEntity.isSubmergedInWater()) {
-            float rotation = ((float)itemEntity.getAge() + partialTicks) / 20.0F + itemEntity.hoverHeight;
-            matrixStack.multiply(Vector3f.POSITIVE_Z.getRadialQuaternion(rotation));
-            rotator.setRotation(new Vec3d(0, 0, rotation));
-        } else if(itemEntity.getStack().getItem() instanceof AliasedBlockItem){
-            matrixStack.multiply(Vector3f.POSITIVE_Z.getRadialQuaternion((float) rotator.getRotation().z));
-        } else if(itemEntity.getStack().getItem() instanceof BlockItem) {
-            matrixStack.multiply(Vector3f.POSITIVE_X.getRadialQuaternion(300));
-            matrixStack.multiply(Vector3f.POSITIVE_Y.getRadialQuaternion((float) rotator.getRotation().z));
-            matrixStack.multiply(Vector3f.POSITIVE_Z.getRadialQuaternion(0));
-        } else {
-            matrixStack.multiply(Vector3f.POSITIVE_Z.getRadialQuaternion((float) rotator.getRotation().z));
+        // Certain BlockItems (Grass Block, Jukebox, Dirt, Ladders) are fine being rotated 180 degrees like standard items.
+        // Other BlockItems (Carpet, Slab) do not like being rotated and should stay flat.
+        // To determine whether a block should be flat or rotated, we check the collision box height.
+        // Anything that takes up more than half a block vertically is rotated.
+        boolean renderBlockFlat = false;
+        if(dropped.getStack().getItem() instanceof BlockItem && !(dropped.getStack().getItem() instanceof AliasedBlockItem)) {
+            Block b = ((BlockItem) dropped.getStack().getItem()).getBlock();
+            VoxelShape shape = b.getOutlineShape(b.getDefaultState(), dropped.world, dropped.getBlockPos(), ShapeContext.absent());
+
+            // Only blocks with a collision box of <.5 should be rendered flat
+            if(shape.getMax(Direction.Axis.Y) <= .5) {
+                renderBlockFlat = true;
+            }
         }
 
-        // offset rendered stacks vertically
-         // 0.01 barely brings flat items flush to the ground
-         matrixStack.translate(0, 0, 0.01f);
-         if(itemEntity.getStack().getItem() instanceof AliasedBlockItem) {
- 
-         } else if(itemEntity.getStack().getItem() instanceof BlockItem) {
-             // make blocks flush with the ground
-             matrixStack.translate(0, 0, -0.12f);
-         }
+        // Make full blocks flush with ground
+        Item item = dropped.getStack().getItem();
+        if(item instanceof BlockItem && !(item instanceof AliasedBlockItem) && !renderBlockFlat) {
+            // make blocks flush with the ground
+             matrix.translate(0, -0.06, 0);
+        }
+
+        // Give all non-flat items a 90* spin
+        if(!renderBlockFlat) {
+            matrix.translate(0, .185, .0);
+            matrix.multiply(Vector3f.POSITIVE_X.getRadialQuaternion(1.571F));
+            matrix.translate(0, -.185, -.0);
+        }
+
+        // Item is flying through air
+        boolean isAboveWater = dropped.world.getBlockState(dropped.getBlockPos()).getFluidState().getFluid().isIn(FluidTags.WATER);
+        if(!dropped.isOnGround() && (!dropped.isSubmergedInWater() && !isAboveWater)) {
+            float rotation = ((float) dropped.getAge() + partialTicks) / 20.0F + dropped.hoverHeight; // calculate rotation based on age and ticks
+
+            // 90* items/blocks (non-flat) get spin on Z axis, flat items/blocks get spin on Y axis
+            if(!renderBlockFlat) {
+                // rotate renderer
+                matrix.translate(0, .185, .0);
+                matrix.multiply(Vector3f.POSITIVE_Z.getRadialQuaternion(rotation));
+                matrix.translate(0, -.185, .0);
+
+                // save rotation in entity
+                rotator.setRotation(new Vec3d(0, 0, rotation));
+            } else {
+                // rotate renderer
+                matrix.multiply(Vector3f.POSITIVE_Y.getRadialQuaternion(rotation));
+
+                // save rotation in entity
+                rotator.setRotation(new Vec3d(0, rotation, 0));
+
+                // Translate down to become flush with floor
+                matrix.translate(0, -.065, 0);
+            }
+
+            // Carrots/Potatoes/Redstone/other crops in air need vertical offset
+            if(dropped.getStack().getItem() instanceof AliasedBlockItem) {
+                matrix.translate(0, 0, .195);
+            }
+
+            else if (!(dropped.getStack().getItem() instanceof BlockItem)) {
+                // Translate down to become flush with floor
+                matrix.translate(0, 0, .195);
+            }
+        }
+
+        // Carrots/Potatoes/Redstone/other crops on ground
+        else if(dropped.getStack().getItem() instanceof AliasedBlockItem){
+            matrix.translate(0, .185, .0);
+            matrix.multiply(Vector3f.POSITIVE_Z.getRadialQuaternion((float) rotator.getRotation().z));
+            matrix.translate(0, -.185, .0);
+
+            // Translate down to become flush with floor
+            matrix.translate(0, 0, .195);
+        }
+
+        // Ladders/Slabs/Carpet and other short blocks on ground
+        else if(renderBlockFlat) {
+            matrix.multiply(Vector3f.POSITIVE_Y.getRadialQuaternion((float) rotator.getRotation().y));
+
+            // Translate down to become flush with floor
+            matrix.translate(0, -.065, 0);
+        }
+
+
+        // Normal blocks/items on ground
+        else {
+            // Translate normal items down to become flush with floor
+            if (!(dropped.getStack().getItem() instanceof BlockItem)) {
+                matrix.translate(0, 0, .195);
+            }
+
+            matrix.translate(0, .185, .0);
+            matrix.multiply(Vector3f.POSITIVE_Z.getRadialQuaternion((float) rotator.getRotation().z));
+            matrix.translate(0, -.185, .0);
+        }
 
         // special-case soul sand
-        if(itemEntity.world.getBlockState(itemEntity.getBlockPos()).getBlock().equals(Blocks.SOUL_SAND)) {
-            matrixStack.translate(0, 0, -.1);
+        if(dropped.world.getBlockState(dropped.getBlockPos()).getBlock().equals(Blocks.SOUL_SAND)) {
+            matrix.translate(0, 0, -.1);
         }
 
         float scaleX = bakedModel.getTransformation().ground.scale.getX();
@@ -102,12 +178,12 @@ public abstract class ItemEntityRendererMixin extends EntityRenderer<ItemEntity>
             float r = -0.0F * (float)(renderCount) * 0.5F * scaleX;
             x = -0.0F * (float)(renderCount) * 0.5F * scaleY;
             y = -0.09375F * (float)(renderCount) * 0.5F * scaleZ;
-            matrixStack.translate(r, x, y);
+            matrix.translate(r, x, y);
         }
 
         // render each item in the stack on the ground (higher stack count == more items displayed)
         for(int u = 0; u < renderCount; ++u) {
-            matrixStack.push();
+            matrix.push();
 
             // random positioning for rendered items, is especially seen in 64 block stacks on the ground
             if (u > 0) {
@@ -115,30 +191,30 @@ public abstract class ItemEntityRendererMixin extends EntityRenderer<ItemEntity>
                     x = (this.random.nextFloat() * 2.0F - 1.0F) * 0.15F;
                     y = (this.random.nextFloat() * 2.0F - 1.0F) * 0.15F;
                     float z = (this.random.nextFloat() * 2.0F - 1.0F) * 0.15F;
-                    matrixStack.translate(x, y, z);
+                    matrix.translate(x, y, z);
                 } else {
                     x = (this.random.nextFloat() * 2.0F - 1.0F) * 0.15F * 0.5F;
                     y = (this.random.nextFloat() * 2.0F - 1.0F) * 0.15F * 0.5F;
-                    matrixStack.translate(x, y, 0.0D);
-                    matrixStack.multiply(Vector3f.POSITIVE_Z.getRadialQuaternion(this.random.nextFloat()));
+                    matrix.translate(x, y, 0.0D);
+                    matrix.multiply(Vector3f.POSITIVE_Z.getRadialQuaternion(this.random.nextFloat()));
                 }
             }
 
             // render item
-            this.itemRenderer.renderItem(itemStack, ModelTransformation.Mode.GROUND, false, matrixStack, vertexConsumerProvider, i, OverlayTexture.DEFAULT_UV, bakedModel);
+            this.itemRenderer.renderItem(itemStack, ModelTransformation.Mode.GROUND, false, matrix, vertexConsumerProvider, i, OverlayTexture.DEFAULT_UV, bakedModel);
 
             // end
-            matrixStack.pop();
+            matrix.pop();
 
             // translate based on scale, which gies vertical layering to high stack count items
             if (!hasDepthInGui) {
-                matrixStack.translate(0.0F * scaleX, 0.0F * scaleY, 0.0625F * scaleZ);
+                matrix.translate(0.0F * scaleX, 0.0F * scaleY, 0.0625F * scaleZ);
             }
         }
 
         // end
-        matrixStack.pop();
-        super.render(itemEntity, f, partialTicks, matrixStack, vertexConsumerProvider, i);
+        matrix.pop();
+        super.render(dropped, f, partialTicks, matrix, vertexConsumerProvider, i);
         callback.cancel();
     }
 }
